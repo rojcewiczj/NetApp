a5b067e697b0067df0154a28d0b6a9d733299a37
John Rojcewicz <rojcewiczj@gmail.com>
2020-11-01 17:12:33 -0600

    added migrations/database

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
