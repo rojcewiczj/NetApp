dad32ffb7eb96dd2e4984c48098bb44a568a1e9f
John Rojcewicz <rojcewiczj@gmail.com>
2020-11-07 17:28:56 -0600

    Set up a system to generate a JWT token for the user on login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -9,6 +9,9 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
+using API.Interfaces;
+using API.Services;
+using API.DTOs;
 
 namespace API.Controllers
 {
@@ -17,15 +20,18 @@ namespace API.Controllers
     public class AccountController : BaseController
     {
         private readonly DataContext _context;
+        
+        private readonly ITokenService _tokenService;
 
-        public AccountController(DataContext context)
-        {
+        public AccountController(DataContext context, ITokenService tokenService)
+        {   
+            _tokenService = tokenService;
             _context = context;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(DTOs.RegisterDto registerDto)
-        {   
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
             if (await UserExists(registerDto.Username))
             {
                 return BadRequest("Username is taken");
@@ -34,15 +40,42 @@ namespace API.Controllers
 
             var user = new AppUser
             {
-                Username = registerDto.Username.ToLower(),
+                Username = registerDto.Username,
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+             return new UserDto
+             {
+                 Username = user.Username,
+                 Token = _tokenService.CreateToken(user)
+             };
 
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.Username == loginDto.Username);
+
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                    return Unauthorized("Invalid password");
+            }
+            return new UserDto
+             {
+                 Username = user.Username,
+                 Token = _tokenService.CreateToken(user)
+             };
         }
 
         private async Task<bool> UserExists(string username)
---
+++ API/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using API.Entities;
+using System.IdentityModel.Tokens;
+namespace API.Interfaces
+{
+public interface ITokenService
+{
+        string CreateToken(AppUser user);
+}
+ }
